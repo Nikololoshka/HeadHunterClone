cc629a7bc3e92e80460a7d1268c8cb7200b2cb42
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2024-09-27 11:33:53 +0300

    Добавлено меню

--- app/src/main/java/com/vereshchagin/nikolay/hh_clone/MainActivity.kt
+++ app/src/main/java/com/vereshchagin/nikolay/hh_clone/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.vereshchagin.nikolay.hh_clone
 
 import android.os.Bundle
+import android.service.voice.VoiceInteractionSession.ActivityId
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.vereshchagin.nikolay.hh_clone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigation()
+        setupWindowInsets()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+    private fun setupWindowInsets() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
