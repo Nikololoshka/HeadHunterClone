800122c21e4bdf1eb6609d7ae0d595733c2d6162
Nikolay Vereshchagin <nick.vereshchagin@gmail.com>
2024-09-27 11:03:55 +0300

    Инициализация: создана начальная структура

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "hh_clone"
+include(":app")
+include(":core")
+include(":feature:home_impl")
+include(":feature:favorite_impl")
+include(":module_injector")
